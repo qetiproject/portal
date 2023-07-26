b811506b74c651b51e0bcc05a14def74f5a8438f
Qeti Khetsuriani <ketideveloper@gmail.com>
2023-07-26 13:09:51 +0400

    update v

--- Portal.Portal.Web/src/app/modules/authentication-module/guard/auth-guard.ts
+++ Portal.Portal.Web/src/app/modules/authentication-module/guard/auth-guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/infrastructure/CustomApi/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private router: Router, private authService: AuthService) { }
 
